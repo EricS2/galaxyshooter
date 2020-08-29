e58ac72c86dbeb0ca1c465b3987560b05827d63f
EricS2 <eric.tnr@gmail.com>
2020-08-29 07:58:38 -0300

    Tiro implementado.

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //se move na velocidade 10
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        //se a posição y do laser é maior ou igual 6
+        //destroi o laser
+        if (transform.position.y >= 6 )
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 5.0f;
-    
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _fireRate = 0.25f;
+    private float _canFire = 0.0f;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //chama o nome dos objetos em que os script esta vinculado
-        //Debug.Log("Name:"+ name);
-
-        //chama a posição x do objeto em que o scrip esta vinculado
-        //Debug.Log("x pos: " + transform.position.x);
-
-        //transform é a posição, rotação e escala do objeto
-        //posição = nova posição
         transform.position = new Vector3(0,0,0);
     }
 
@@ -29,30 +25,43 @@ public class Player : MonoBehaviour
     {
         Movement();
 
+        //se espaço ou click é pressionado
+        //disparar o laser
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0))
+        {
+            //Time.time = é o tempo em segundos desde que a aplicação foi iniciada
+            // ou seja 3 minutos jogo Time.Time será 180 segundos
+            if (Time.time > _canFire)
+            {
+              Shoot();
+            }
+        }
+
     }
 
+    private void Shoot()
+    {
+          //renderizar a prefab  na posição do jogador
+                //Instantiate(nome_prefab, posição do jogador, mantém o valor de rotação) 
+                // Instantiate(laserPrefab, transform.position, Quaternion.identity);
+                
+                //renderizar prefab acima do jogador
+                Instantiate(_laserPrefab, transform.position + new Vector3(0,0.88f,0),Quaternion.identity);
+                
+                //cooldown
+                _canFire = Time.time + _fireRate;
+    }
 
     private void Movement()
     {
-                //o objeto se move a 1 metro por segundo.
-        //transform.Translate(Vector3.right * Time.deltaTime);
-
-        //o objeto se move a 5 metros por segundo.
-        //transform.Translate(Vector3.right * Time.deltaTime * 5);    
-        
-        //novo vetor(-5 ou -1?,0,0) * 1 * velocidade
-        // transform.Translate(Vector3.right * Time.deltaTime * speed);
-
         //movimentação básica
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime );
-        transform.Translate(Vector3.up * speed * verticalInput *  Time.deltaTime );
+        transform.Translate(Vector3.right * _speed * horizontalInput * Time.deltaTime );
+        transform.Translate(Vector3.up * _speed * verticalInput *  Time.deltaTime );
         
         //limitar area do jogador (eixo vertical)
-        // se o jogador_y é maior q 0
-        //jogador_y fica em 0 em Y
         if(transform.position.y > 0)
         {
             transform.position = new Vector3(transform.position.x, 0, 0);
@@ -62,8 +71,7 @@ public class Player : MonoBehaviour
         {
         transform.position = new Vector3(transform.position.x, -4.2f ,0);
         }
-
-        //se a posição de x é maior q 9.5
+        //transição do jogador p/ outro lado da tela
         //eixo horizontal
         if (transform.position.x > 9.5f)
         {
